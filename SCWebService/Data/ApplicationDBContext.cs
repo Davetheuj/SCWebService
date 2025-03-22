13896b3151caab140272d1f5a9ca4709e28fc409
David U <davetheuj@gmail.com>
2025-03-22 13:17:24 -0700

    Add DbContext and User model

---
+++ SCWebService/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SCWebService.Models.Entities;
+
+namespace SCWebService.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
+ 
---
+++ SCWebService/Models/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿namespace SCWebService.Models.Entities
+{
+    public class User
+    {
+        public Guid ID { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string Email { get; set; }
+
+        public required string Password { get; set; }
+
+    }
+}
