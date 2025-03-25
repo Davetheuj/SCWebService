ddc5cd449def7860627b28dc4c860914c7eaad0c
David U <davetheuj@gmail.com>
2025-03-25 13:02:42 -0700

    Remove Entity Framework and Azure SQL Server From Project

---
+++ SCWebService/Models/Entities/BoardPreset.cs
@@ -0,0 +1,10 @@
+﻿namespace SCWebService.Models.Entities
+{
+    public class BoardPreset
+    {
+        public required string UserID { get; set; }
+        public required List<int> XVals { get; set; }
+        public required List<int> YVals { get; set; }
+        public required List<int> PieceTypes { get; set; }
+    }
+}
